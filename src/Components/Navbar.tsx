4c1b7acdc2e6b93c7a9706836d649c99355703dd
CubeMyst <yeiser192@gmail.com>
2023-09-04 14:59:20 -0400

    Update Navbar.tsx

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,4 +1,4 @@
-import { IconSun, IconMoon } from "./Icons";
+import { IconMoon, IconSun } from "./Icons";
 
 interface NavTypes {
   isDarkMode: boolean;
@@ -6,12 +6,10 @@ interface NavTypes {
 }
 
 export default function Navbar({ isDarkMode, toggle }: NavTypes) {
+  const background = isDarkMode ? "bg-dark-blue text-white" : "bg-white text-black"
+
   return (
-    <header
-      className={`${
-        isDarkMode ? "bg-dark-blue text-white" : "bg-white text-black"
-      } shadow mb-20 absolute top-0 w-full`}
-    >
+    <header className={`${background} shadow mb-20 absolute top-0 w-full`}>
       <nav className="flex justify-between font-bold py-6 px-7">
         <h1 className="text-2xl">Where in the world?</h1>
         <button type="button" onClick={toggle}>
